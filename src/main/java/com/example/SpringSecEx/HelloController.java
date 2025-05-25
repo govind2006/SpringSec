88597d8923cfeacb874c612ea7641c2527fd761b
GOVIND KUMAR <39936384+govind2006@users.noreply.github.com>
2025-05-25 10:10:50 +0000

    hello

---
+++ src/main/java/com/example/SpringSecEx/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.SpringSecEx;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet() {
+        return "Hello, World!";
+    }
+}
