0da80dfaecc1a4e7313daa37080d4bd6a7531b8c
GOVIND KUMAR <39936384+govind2006@users.noreply.github.com>
2025-05-28 10:59:08 +0000

    db

---
+++ src/main/java/com/example/SpringSecEx/controller/HelloController.java
@@ -0,0 +1,17 @@
+
+package com.example.SpringSecEx.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request) {
+        return "Hello, World! " + request.getSession().getId();
+    }
+}
